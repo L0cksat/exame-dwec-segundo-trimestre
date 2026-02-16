8414dab0bae0e1ff77bb3eb56f79bc253e6dd554
Nicky Jones De Giorgi <stephennicholas.jones266@comunidadunir.net>
2026-02-16 11:12:50 +0100

    Update 1: Subida inicial del proyecto del examen

---
+++ app-series-examen/src/app/pages/new/new.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-new',
+  imports: [RouterLink],
+  templateUrl: './new.html',
+  styleUrl: './new.css',
+})
+export class NewComponent {
+
+}
